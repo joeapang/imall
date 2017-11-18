f966e1de62023d3677828276f8724f17fd433f22
pFW3251 <pangwenjing@gmail.com>
2017-11-18 17:37:05 +0800

    使通配置mybatisgenerator

---
+++ src/main/java/com/imall/dao/OrderMapper.java
@@ -0,0 +1,17 @@
+package com.imall.dao;
+
+import com.imall.pojo.Order;
+
+public interface OrderMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Order record);
+
+    int insertSelective(Order record);
+
+    Order selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Order record);
+
+    int updateByPrimaryKey(Order record);
+}
---
+++ src/main/java/com/imall/dao/ProductMapper.java
@@ -0,0 +1,17 @@
+package com.imall.dao;
+
+import com.imall.pojo.Product;
+
+public interface ProductMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Product record);
+
+    int insertSelective(Product record);
+
+    Product selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Product record);
+
+    int updateByPrimaryKey(Product record);
+}
